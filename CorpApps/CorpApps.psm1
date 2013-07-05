23cecfd564c0b5d84af4e88fa39f09216380c2b6
Michael <michaellwest@gmail.com>
2013-07-05 15:20:10 -0500

    Create CorpApps.psm1
    Root module with default settings.

---
+++ CorpApps/CorpApps.psm1
@@ -0,0 +1,6 @@
+#Define all the variables here.
+
+$global:PSDefaultParameterValues = @{
+    "Send-MailMessage:From"="CorpApps Team <corpapps@popularcompanyname.com >"
+    "Send-MailMessage:SmtpServer"="mail.popularcompanyname.com"
+}
